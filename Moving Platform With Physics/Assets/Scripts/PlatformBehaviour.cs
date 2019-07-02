b07cc21503de01a8eed90845150e4873fb2f6d99
Muhammed Halil Ayhan <ayhannmuhammed@gmail.com>
2019-07-02 22:39:07 +0300

    First Commit

---
+++ Moving Platform With Physics/Assets/Scripts/PlatformBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour
+{
+	public Transform startPoint;
+	public Transform endPoint;
+	public float travelTime;
+	private Rigidbody rb;
+	private Vector3 currentPos;
+
+	CharacterController cc;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate()
+	{
+		currentPos = Vector3.Lerp(startPoint.position, endPoint.position,
+			Mathf.Cos(Time.time / travelTime * Mathf.PI * 2) * -.5f + .5f);
+		rb.MovePosition(currentPos);
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+			cc = other.GetComponent<CharacterController>();
+	}
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player")
+			cc.Move(rb.velocity * Time.deltaTime);
+	}
+
+}
---
+++ Moving Platform With Physics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private CharacterController controller;
+	public float moveSpeed = 6f,gravity = 20f,jumpForce = 8f;
+	private Vector3 moveDir = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+		controller = GetComponent<CharacterController>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.JoystickButton0))
+			moveDir.y = jumpForce;
+		moveDir.y -= gravity;
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(moveDir * moveSpeed);
+	}
+}
